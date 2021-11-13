add992d021b5b93ef51759b3b0d398588ea72981
Gesendex <gireevtu@gmail.com>
2021-11-14 00:38:10 +0300

    Добавление основных библиотек

---
+++ Recycle.Models/Role.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Recycle.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ Recycle.Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Recycle.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
+
+    }
+}
