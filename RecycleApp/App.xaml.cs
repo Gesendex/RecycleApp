24a5df7e45d5ffffe98f47c69ed52cc3a4f24d79
Gesendex <gireevtu@gmail.com>
2021-11-23 04:14:48 +0300

    Было сверстано окно авторизации/регистрации и страницы для авторизации, регистрации и реализован переход между ними

--- RecycleApp/App.xaml.cs
+++ RecycleApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recycle.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace RecycleApp
     /// </summary>
     public partial class App : Application
     {
+        public Client CurrentUser { get; set; } = null;
     }
 }
